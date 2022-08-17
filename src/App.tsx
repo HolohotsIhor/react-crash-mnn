7c5e12c8ebae078092ed9412805fc50cff320bc6
HolohotsIhor <gologocigor@gmail.com>
2022-08-17 13:13:44 +0300

    Created project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {Product} from './components/Product'
+import {products} from './data/products'
+import axios from "axios";
+import {IProduct} from "./models";
 
 function App() {
+    const [products, setProducts] = useState<IProduct[]>([]);
+    const [loading, setLoading] = useState(false);
+    // https://youtu.be/OJ16BaPC6VI?t=3424
+    // const [error, setError] = useState('');
+
+    async function fetchProducts() {
+        setLoading(true);
+        const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
+        setProducts(response.data);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+        }, []
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+        className='container mx-auto max-w-2xl pt-5'
+    >
+        {loading && <p className='text-center'>Loading...</p>}
+        {products.map( product => <Product product={product} key={product.id} /> )}
     </div>
   );
 }
